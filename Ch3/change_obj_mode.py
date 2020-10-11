87c9002781f4c8497b32821276042aa9a977d135
Isabel Lupiani <isabel.lupiani@gmail.com>
2020-10-11 12:01:49 -0400

    update ch3 change_obj_mode.py to 2.91, change behavior to looking up cube and setting it as active, before deselecting everything else and changing it to Edit mode

--- Ch3/change_obj_mode.py
+++ Ch3/change_obj_mode.py
@@ -1,8 +1,9 @@
 import bpy
 
-bpy.ops.object.mode_set(mode = 'OBJECT')
-cube = bpy.context.scene.objects['Cube']
-bpy.context.view_layer.objects.active = cube
-cube.select_set(True)
-bpy.ops.object.mode_set(mode = 'EDIT')
-bpy.context.view_layer.update()
+if bpy.context.scene.objects.find('Cube') >= 0:
+    cube = bpy.context.scene.objects['Cube']
+    bpy.context.view_layer.objects.active = cube
+    bpy.ops.object.mode_set(mode = 'OBJECT')
+    bpy.ops.object.select_all(action = 'DESELECT')
+    bpy.ops.object.mode_set(mode = 'EDIT')
+    bpy.context.view_layer.update()
